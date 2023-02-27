9ce93131076282afcdf931d43c20ed47544fc4cd
Muhammad Sheikh <muhammad-sheikh@outlook.com>
2023-02-27 11:20:20 -0500

    finished 5 problems with variables and scanners

--- src/VariablesScanners.java
+++ src/VariablesScanners.java
@@ -3,11 +3,50 @@ import java.util.Scanner;
 public class VariablesScanners {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+
+        /*
+        Problem #1
         System.out.println("What is your name?");
         String input = userInput.nextLine();
         System.out.println("Hello s" + input + "!");
+         */
 
+        /*
+        //Problem #2
+        System.out.println("What is your Favourite food?");
+        String fav;
+        String leastFav;
+        fav = userInput.nextLine();
+        System.out.println("Whats your least favourite food?");
+        leastFav = userInput.nextLine();
+        System.out.println("I love " + fav + "!" + " You're right, " +  leastFav + "is disgusting!");
+         */
 
+        /*
+        //Problem #3
+        System.out.println("Enter 3 Numbers:");
+        String num1 = userInput.nextLine();
+        String num2 = userInput.nextLine();
+        String num3 = userInput.nextLine();
+        System.out.println(num3 + "," + num2 + "," + num1);
+         */
 
+        /*
+        //Problem 4
+        System.out.println("Give me 2 numbers!");
+        int number1 = userInput.nextInt();
+        int number2 = userInput.nextInt();
+        System.out.println("The output is: " + (number1 + number2));
+        */
+
+        /*
+        //Problem 5
+        System.out.println("Please enter your 4 grades:");
+        double grade1 = userInput.nextDouble();
+        double grade2 = userInput.nextDouble();
+        double grade3 = userInput.nextDouble();
+        double grade4 = userInput.nextDouble();
+        System.out.println("Your average is: " + ((grade1 +grade2 +grade3 + grade4)/4));
+         */
     }
 }
